05c674a27dbfd20270632dadd67908b06d8526b0
Luis Polanco <lpolanco2319@gmail.com>
2019-11-19 18:16:51 -0400

    Visualizacion de miembros-ConsultaView

---
+++ WebApp-Multifuncional/WebApp-Multifuncional/Controllers/ConsultaController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp_Multifuncional.Models.ModelConsulta;
+
+namespace WebApp_Multifuncional.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        // GET: Consulta
+        public ActionResult Consulta()
+        {
+			/*Miembro Dropdownlist*/
+			List<ConsultaTabla> lst = null;
+			using (Models.multifuncionalEntities2 db = new Models.multifuncionalEntities2())
+			{
+				 lst =
+					(from d in db.miembro
+					 select new ConsultaTabla
+					 {
+						 Id = d.id,
+						 Matricula = d.matricula
+
+					 }).ToList();
+			}
+
+			List<SelectListItem> items = lst.ConvertAll(a =>
+			{
+				return new SelectListItem()
+				{
+					Text = a.Matricula.ToString(),
+					Value = a.Id.ToString(),
+					Selected = false
+				};
+			});
+
+			ViewBag.items = items;
+			/*End Miembro Dropdownlist*/
+
+			return View(items);
+		}
+    }
+}
