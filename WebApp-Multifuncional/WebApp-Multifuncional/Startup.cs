635c869f883d3119d5ac939a8ba610ef106594f4
Luis Polanco <lpolanco2319@gmail.com>
2019-11-19 13:33:10 -0400

    Configuracion y Login Ready

---
+++ WebApp-Multifuncional/WebApp-Multifuncional/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp_Multifuncional.Startup))]
+namespace WebApp_Multifuncional
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
